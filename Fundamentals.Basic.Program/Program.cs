0c273d2ab2b09cd6ea593f1de317895953e6a8a0
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2023-01-16 08:35:03 +0100

    basic sum calculation program

--- Fundamentals.Basic.Program/Program.cs
+++ Fundamentals.Basic.Program/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        var numbers = new int[] { 1, 2, 3, 4, 5 };
+        var sum = 0;
+
+        for (var n = 0; n < numbers.Length; n++)
+        {
+            sum += numbers[n];
+        }
+        Console.WriteLine(sum);
+    }
+}
