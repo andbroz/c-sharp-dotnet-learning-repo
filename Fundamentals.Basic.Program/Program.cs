83ad2b893245ec6369d59eca44a7c3cb4a56e729
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2023-01-19 20:25:16 +0100

    list example

--- Fundamentals.Basic.Program/Program.cs
+++ Fundamentals.Basic.Program/Program.cs
@@ -2,28 +2,15 @@
 {
     static void Main()
     {
+        System.Collections.Generic.List<int> list = new List<int>();
 
-        Point p1 = new Point(1,2);
-        Point p2;
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
 
-        p2 = p1;
-        p2.y = 3;
-
-        
-        Console.WriteLine($"p1 is {p1.y} and p2 is {p2.y}");
-      
-        
-    }
-
-    public struct Point
-    {
-        public int x;
-        public int y;
-
-        public Point(int x, int y)
+        foreach(int item in list)
         {
-            this.x = x;
-            this.y = y;
+            Console.WriteLine(item);
         }
     }
 }
