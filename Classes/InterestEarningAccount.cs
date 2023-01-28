49288c9937b18198ae8b4d00b03ecfb1865fa591
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2023-01-28 22:18:25 +0100

    intro to classes and OOP

---
+++ Classes/InterestEarningAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class InterestEarningAccount:BankAccount
+    {
+        public InterestEarningAccount(string name, decimal initialBalance) : base(name, initialBalance)
+        {
+        }
+
+        public override void PerformMonthEndTransactions()
+        {
+            if (Balance > 500m)
+            {
+                decimal interest = Balance * 0.05m;
+                MakeDeposit(interest, DateTime.Now, "apply monthly interest");
+            }
+
+        }
+    }
+}
---
+++ Classes/LineOfCreditAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class LineOfCreditAccount : BankAccount
+    {
+        public LineOfCreditAccount(string name, decimal initialBalance, decimal creditLimit) : base(name, initialBalance, -creditLimit)
+        {
+        }
+
+        public override void PerformMonthEndTransactions()
+        {
+            if (Balance < 0)
+            {
+                // Negate the balance to get a positive interest charge:
+                decimal interest = -Balance * 0.07m;
+                MakeWithdrawal(interest, DateTime.Now, "Charge monthly interest");
+            }
+        }
+
+        protected override Transaction? CheckWithdrawalLimit(bool isOverdrawn) =>
+            isOverdrawn
+            ? new Transaction(-20, DateTime.Now, "Apply overdraft fee")
+            : default;
+    }
+}
