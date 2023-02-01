9f483c2f07a6e66cbb244694ae1491067c6ab155
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2023-02-01 22:21:43 +0100

    exception handling

---
+++ Error.Handling/ExceptionsDemo.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Error.Handling
+{
+    public static class ExceptionsDemo
+    {
+        private static bool s_end = false;
+
+        public static void DoException(string? inputText)
+        {
+
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Type a key: ");
+                    var input = Console.ReadKey();
+                    Console.WriteLine();
+                    Console.Write("Your input was:");
+
+                    Console.WriteLine(input.KeyChar);
+
+                    if (input.KeyChar.Equals('E'))
+                    {
+                        Console.WriteLine("Ups something went wrong.");
+                        throw new ArgumentException($"{nameof(input.Key)} wrong key pressed");
+                    }
+                    if (input.KeyChar.Equals('n'))
+                    {
+                        Console.WriteLine("Ups something went wrong.");
+                        throw new NotImplementedException("error here");
+                    }
+                    if (input.KeyChar.Equals('k'))
+                    {
+                        Console.WriteLine("Ups something went wrong.");
+                        throw new InvalidOperationException("error here");
+                    }
+                    if (input.KeyChar.Equals('o'))
+                    {
+                        Console.WriteLine("Ups something went wrong.");
+                        throw new ArgumentOutOfRangeException(nameof(inputText), "Input text should not be empty");
+                    }
+
+                    if (input.KeyChar.Equals('q'))
+                    {
+                        s_end = true;
+                        Console.WriteLine("Exiting program");
+                    }
+
+
+
+                }
+                // implement catch statement from most specific exception type
+                // to the least specific exception type at the bottom
+                catch (ArgumentException error)
+                {
+                    Console.WriteLine(error.ToString());
+                }
+                catch (NotImplementedException e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+                // the least specific exception
+                catch (Exception e)
+                {
+                    // handle all other exceptions
+                    Console.WriteLine(e.ToString());
+                }
+                // after all exceptions we can implement signle finally block
+                finally
+                {
+                    Console.WriteLine("Finally all is good. We made it to the end.");
+                }
+
+            } while (!s_end);
+
+        }
+
+    }
+}
---
+++ PatternMatchingDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿
+int? maybe = 12;
+
+
+// declaration pattern to test the type of variable and assign it to new variable
+if(maybe is int number)
+{
+    Console.WriteLine($"The nullable int 'maybe' has the value {number}");
+} else
+{
+    Console.WriteLine("The nullable int 'maybe' doesn't hold a value");
+}
+
+
+string? message = "This is not the null string";
+
+
+// check that varibale is not null
+// constant pattern to compare with `null` and with logical pattern `not` that matches when negated pattern doesn't match
+if(message is not null)
+{
+    Console.WriteLine(message);
+}
+
+string WaterState(int tempInFahrenheit) =>
+    tempInFahrenheit switch
+    {
+        (> 32) and (< 212) => "liquid",
+        < 32 => "solid",
+        > 212 => "gas",
+        32 => "solid/liquid transition",
+        212 => "liquid / gas transition",
+    };
+
+string state = WaterState(99);
+
+Console.WriteLine($"Water in 99F has {state} state.");
