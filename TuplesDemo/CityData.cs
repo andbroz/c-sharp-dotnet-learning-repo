15f10fc7f976934bcb1414b40970255b51f287e6
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2023-02-02 21:45:23 +0100

    customr deconstruct for tuple

--- TuplesDemo/CityData.cs
+++ TuplesDemo/CityData.cs
@@ -34,5 +34,33 @@ namespace TuplesDemo
         {
             return (City, 134976);
         }
+
+        public (string, double, int, int, int, int) QueryCityDataForYears(string name, int year1, int year2)
+        {
+            int population1 = 0, population2 = 0;
+            double area = 0;
+
+            if (name == "New York City")
+            {
+                area = 468.48;
+                if (year1 == 1960)
+                {
+                    population1 = 7781984;
+                }
+                if (year2 == 2010)
+                {
+                    population2 = 8175133;
+                }
+                return (name, area, year1, population1, year2, population2);
+            }
+
+            return ("", 0, 0, 0, 0, 0);
+        }
+
+        public void Deconstruct(out string city, out string country)
+        {
+            city = City;
+            country = Country;
+        }
     }
 }
--- TuplesDemo/Program.cs
+++ TuplesDemo/Program.cs
@@ -13,3 +13,16 @@ Console.WriteLine($"{cityName} is placed in {countryName}");
 Console.WriteLine($"{cityCountry} or {countryCity}?");
 
 Console.WriteLine($"{cityNameTwo} has {inhabitants} inhabitants");
+
+
+// skip variables with discards _
+var (_, _, _, pop1, _, pop2) = wroclawCity.QueryCityDataForYears("New York City", 1960, 2010);
+
+Console.WriteLine($"Population change, 1960 to 2010: {pop2 - pop1:N0}");
+
+
+//using custom deconstruct
+
+var (cityNameDe, countryNameDe) = wroclawCity;
+
+Console.WriteLine($"deconstructed {cityNameDe} in {countryNameDe}");
