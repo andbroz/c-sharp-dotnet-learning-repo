422f3977ca9a9750e15fa5ac68ee4b3f42c00623
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2023-02-02 21:00:03 +0100

    tuples demo

---
+++ TuplesDemo/CityData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuplesDemo
+{
+    public class CityData
+    {
+        private string _city;
+        private string _country;
+
+        public CityData() : this("unknown", "unknown") { }
+
+        public CityData(string city, string country) { 
+            _city = city;
+            _country = country;
+        }
+
+        public string City { get { return _city; }  }
+
+        public string Country { get { return _country;} }
+
+        public (string,string,string,string) GetCityData()
+        {
+            string cityCountry = $"{City}-{Country}";
+            string countryCity = $"{Country}-{City}";
+
+            return (City, Country, cityCountry, countryCity);
+        }
+
+        public (string, int) GetInhabitants()
+        {
+            return (City, 134976);
+        }
+    }
+}
---
+++ TuplesDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using TuplesDemo;
+
+Console.WriteLine("Tuple Demo");
+
+CityData wroclawCity = new("Wroclaw", "Polska");
+
+var (cityName, countryName, cityCountry, countryCity ) = wroclawCity.GetCityData();
+
+(string cityNameTwo, int inhabitants) = wroclawCity.GetInhabitants();
+
+Console.WriteLine($"{cityName} is placed in {countryName}");
+Console.WriteLine($"{cityCountry} or {countryCity}?");
+
+Console.WriteLine($"{cityNameTwo} has {inhabitants} inhabitants");
